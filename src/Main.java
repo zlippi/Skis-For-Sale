a7dff058eeabd335694afb56e69d92e261daca8f
Zachary Lippincott <zacharylippincott@gmail.com>
2019-04-22 14:57:34 -0400

    Completely reworked the DataGetter class. Gui now displays names and program runs normally.

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,52 @@
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
 	ListView<String> skiView;
 	DataGetter dataGet;
-	
+	String[] skiData;
+
 	public static void main(String[] args) {
-		
+
 		launch(args);
-		
-		}
+
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		dataGet = new DataGetter();
 		primaryStage.setTitle("Skis");
+
+		skiData = dataGet.getFileData("Skis.csv");
+
 		
-		ArrayList<Ski> skis = dataGet.getFileData("Skis.csv");
-		String[] skiData = new String[skis.size()];
-		for (int i = 0; i < skis.size(); i++) {
-			skiData[i] = skis.get(i).toString();
-		}
 		skiView = new ListView<String>();
-		skiView.getItems().addAll(skiData);
+		for (int i = 0; i < skiData.length; i++) {
+			skiView.getItems().add(getSkiName(skiData[i]));
+		}
 		skiView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		skiView.setPrefSize(120, 120);
 		StackPane layout = new StackPane();
-		
+
 		layout.getChildren().add(skiView);
-		
+
 		Scene scene = new Scene(layout, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
-		
-		
+
+	}
+	
+	private String getSkiName(String dat) {
+		dat = dat.split(",")[2].replace("\"", "");
+		dat = dat.replace(",", "");
+		return dat;
 	}
 
 }
--- src/Ski.java
+++ src/Ski.java
@@ -1,11 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Ski {
 
