852155d19030955f5e96bd98bb976917f6061bd4
Zachary Lippincott <zacharylippincott@gmail.com>
2019-04-22 17:40:16 -0400

    Working on list selection and displaying text

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Box;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -23,19 +27,44 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		dataGet = new DataGetter();
 		primaryStage.setTitle("Skis");
-
+		
+		Text itemDesc = new Text();
+		Box box = new Box();
+		skiView = new ListView<String>();
+		
+		
+		
 		skiData = dataGet.getFileData("Skis.csv");
 
 		
-		skiView = new ListView<String>();
+		
 		for (int i = 0; i < skiData.length; i++) {
 			skiView.getItems().add(getSkiName(skiData[i]));
 		}
 		skiView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		skiView.setPrefSize(120, 120);
+		skiView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				
+				itemDesc.setText(skiData[skiView.getSelectionModel().getSelectedIndex()]);
+				
+			}
+			
+			
+			
+			
+		});
 		StackPane layout = new StackPane();
 
+		
+		
+		
+		
+		
 		layout.getChildren().add(skiView);
+		layout.getChildren().add(itemDesc);
 
 		Scene scene = new Scene(layout, 300, 250);
 		primaryStage.setScene(scene);
